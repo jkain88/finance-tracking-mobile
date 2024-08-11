f9c4d7575882e846720af819a4fe77233c73c64d
Martin <torricermartinjosh@gmail.com>
2024-08-11 20:22:19 +0800

    Clean up

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -12,12 +12,7 @@ WebBrowser.maybeCompleteAuthSession();
 export default function LoginScreen() {
   const navigation = useNavigation<NavigationProp<RootStackParamList>>();
   const handleLogin = async () => {
-    const redirectUri =
-      Constants.appOwnership === 'expo'
-        ? AuthSession.makeRedirectUri({ scheme: 'finance-tracking-mobile' })
-        : 'finance-tracking-mobile://auth';
     const result = await WebBrowser.openAuthSessionAsync(
-      // 'http://localhost:8080/api/v1/auth/google',
       'https://finance-tracking-production.up.railway.app/api/v1/auth/google',
       'finance-tracking-mobile://auth'
     );
